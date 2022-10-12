d9a81e0f5e574778d25031b5b5034400e359ed0d
Renan Torres <pesterenan@gmail.com>
2022-10-12 01:27:21 -0300

    [DEV]: Utilities file refactored

---
+++ src/com/pesterenan/model/ActiveVessel.java
@@ -0,0 +1,126 @@
+package com.pesterenan.model;
+
+import com.pesterenan.MechPeste;
+import com.pesterenan.resources.Bundle;
+import com.pesterenan.views.StatusJPanel;
+import krpc.client.Connection;
+import krpc.client.RPCException;
+import krpc.client.Stream;
+import krpc.client.StreamException;
+import krpc.client.services.KRPC.GameScene;
+import krpc.client.services.SpaceCenter;
+import krpc.client.services.SpaceCenter.*;
+
+import java.util.Map;
+
+import static com.pesterenan.views.StatusJPanel.isBtnConnectVisible;
+import static com.pesterenan.views.StatusJPanel.setStatus;
+
+public class ActiveVessel {
+
+protected final static float CONST_GRAV = 9.81f;
+
+protected static SpaceCenter centroEspacial;
+private static Connection conexao;
+protected Vessel naveAtual;
+protected AutoPilot ap;
+protected Flight parametrosDeVoo;
+protected ReferenceFrame pontoRefOrbital, pontoRefSuperficie;
+protected Stream<Float> massaTotal;
+protected float bateriaTotal, gravityAcel;
+protected CelestialBody currentBody;
+
+protected Map<String, String> commands;
+protected Stream<Double> altitude, altitudeSup, apoastro, periastro, velVertical, tempoMissao, velHorizontal;
+
+
+public ActiveVessel(Connection con) {
+	setConexao(con);
+	initializeParameters();
+}
+
+public static Connection getConexao() {
+	return conexao;
+}
+
+private void setConexao(Connection con) {
+	conexao = con;
+}
+
+private void initializeParameters() {
+	try {
+		centroEspacial = SpaceCenter.newInstance(getConexao());
+		naveAtual = centroEspacial.getActiveVessel();
+		ap = naveAtual.getAutoPilot();
+		currentBody = naveAtual.getOrbit().getBody();
+		pontoRefOrbital = currentBody.getReferenceFrame();
+		pontoRefSuperficie = naveAtual.getSurfaceReferenceFrame();
+		parametrosDeVoo = naveAtual.flight(pontoRefOrbital);
+		massaTotal = getConexao().addStream(naveAtual, "getMass");
+	} catch (RPCException | StreamException e) {
+		checarConexao();
+	}
+}
+
+protected void checarConexao() {
+	try {
+		if (MechPeste.getCurrentGameScene().equals(GameScene.FLIGHT)) {
+			naveAtual = centroEspacial.getActiveVessel();
+			setStatus(Bundle.getString("status_connected"));
+			isBtnConnectVisible(false);
+		}
+		else {
+			setStatus(Bundle.getString("status_ready"));
+		}
+	} catch (RPCException | NullPointerException e) {
+		setStatus(Bundle.getString("status_error_connection"));
+		isBtnConnectVisible(true);
+	}
+}
+
+protected void throttle(float acel) throws RPCException {
+	naveAtual.getControl().setThrottle(acel);
+}
+
+protected void throttle(double acel) throws RPCException {
+	throttle((float) acel);
+}
+
+protected void liftoff() throws InterruptedException {
+	try {
+		naveAtual.getControl().setSAS(true);
+		throttle(1f);
+		if (naveAtual.getSituation().equals(VesselSituation.PRE_LAUNCH)) {
+			float launchCount = 5f;
+			while (launchCount > 0) {
+				StatusJPanel.setStatus(String.format(Bundle.getString("status_launching_in"), launchCount));
+				launchCount -= 0.1;
+				Thread.sleep(100);
+			}
+			naveAtual.getControl().activateNextStage();
+		}
+		setStatus(Bundle.getString("status_liftoff"));
+	} catch (RPCException erro) {
+	}
+}
+
+protected double getTWR() throws RPCException, StreamException {
+	return naveAtual.getAvailableThrust() / ((massaTotal.get() * gravityAcel));
+}
+
+protected double getMaxAcel() throws RPCException, StreamException {
+	return getTWR() * gravityAcel - gravityAcel;
+}
+
+protected void disengageAfterException(String statusMessage) {
+	try {
+		StatusJPanel.setStatus(statusMessage);
+		ap.setReferenceFrame(pontoRefSuperficie);
+		ap.disengage();
+		throttle(0);
+		Thread.sleep(3000);
+		StatusJPanel.setStatus(Bundle.getString("status_ready"));
+	} catch (Exception ignored) {
+	}
+}
+}
---
+++ src/com/pesterenan/utils/PathFinding.java
@@ -0,0 +1,211 @@
+package com.pesterenan.utils;
+
+import com.pesterenan.model.ActiveVessel;
+import krpc.client.Connection;
+import krpc.client.RPCException;
+import krpc.client.services.Drawing;
+import krpc.client.services.SpaceCenter;
+import krpc.client.services.SpaceCenter.Waypoint;
+import krpc.client.services.SpaceCenter.WaypointManager;
+import org.javatuples.Triplet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathFinding extends ActiveVessel {
+
+public static final Vector L30DEG = new Vector(Math.sin(Math.toRadians(30)), Math.cos(Math.toRadians(30)), 0.0);
+public static final Vector L60DEG = new Vector(Math.sin(Math.toRadians(60)), Math.cos(Math.toRadians(60)), 0.0);
+public static final Vector L90DEG = new Vector(Math.sin(Math.toRadians(90)), Math.cos(Math.toRadians(90)), 0.0);
+public static final Vector R30DEG = new Vector(-Math.sin(Math.toRadians(30)), Math.cos(Math.toRadians(30)), 0.0);
+public static final Vector R60DEG = new Vector(-Math.sin(Math.toRadians(60)), Math.cos(Math.toRadians(60)), 0.0);
+public static final Vector R90DEG = new Vector(-Math.sin(Math.toRadians(90)), Math.cos(Math.toRadians(90)), 0.0);
+private static final float SEARCHING_DISTANCE = 4400000;
+private WaypointManager waypointManager;
+private List<Waypoint> waypointsToReach;
+private List<Vector> pathToTarget;
+private Drawing drawing;
+
+public PathFinding(Connection con) {
+	super(con);
+	initializeParameters();
+}
+
+private void initializeParameters() {
+	try {
+		waypointManager = centroEspacial.getWaypointManager();
+		waypointsToReach = new ArrayList<Waypoint>();
+		pathToTarget = new ArrayList<Vector>();
+		drawing = Drawing.newInstance(getConexao());
+
+	} catch (RPCException e) {
+		throw new RuntimeException(e);
+	}
+}
+
+public void addWaypointsOnSameBody(String waypointName) throws RPCException {
+	for (Waypoint waypoint : waypointManager.getWaypoints()) {
+		boolean hasSameName = waypoint.getName()
+																	.toLowerCase()
+																	.contains(waypointName.toLowerCase());
+		boolean isOnSameBody = waypoint.getBody()
+																	 .equals(currentBody);
+		if (hasSameName && isOnSameBody) {
+			waypointsToReach.add(waypoint);
+		}
+	}
+}
+
+public Vector findNearestWaypoint() throws RPCException, IOException, InterruptedException {
+	double currentDistance = SEARCHING_DISTANCE;
+	Waypoint currentWaypoint = null;
+	for (Waypoint waypoint : waypointsToReach) {
+		double waypointDistance =
+						Vector.distance(new Vector(naveAtual.position(pontoRefOrbital)), waypointPosOnSurface(waypoint));
+		if (currentDistance > waypointDistance) {
+			currentDistance = waypointDistance;
+			currentWaypoint = waypoint;
+		}
+	}
+	return waypointPosOnSurface(currentWaypoint);
+}
+
+private Vector waypointPosOnSurface(Waypoint waypoint) throws RPCException {
+	return new Vector(currentBody.surfacePosition(waypoint.getLatitude(), waypoint.getLongitude(), pontoRefOrbital));
+}
+
+public boolean isPathToTargetEmpty() {
+	return pathToTarget.isEmpty();
+}
+
+public Vector getPathsFirstPoint() {
+	if (isPathToTargetEmpty()) return null;
+	return pathToTarget.get(0);
+}
+
+public void removePathsCurrentPoint() {
+	if (isPathToTargetEmpty()) return;
+	pathToTarget.remove(0);
+}
+
+public void removeWaypointFromList() throws RPCException {
+	if (!waypointsToReach.isEmpty()) {
+		if (!waypointsToReach.get(0)
+												 .getHasContract()) {
+			waypointsToReach.remove(0);
+		}
+	}
+}
+
+/**
+ * Builds the path to the targetPosition, on the Celestial Body Reference ( Orbital Ref )
+ *
+ * @param targetPosition the target pos to build the path to
+ * @throws IOException
+ * @throws RPCException
+ * @throws InterruptedException
+ */
+public void buildPathToTarget(Vector targetPosition) throws IOException, RPCException, InterruptedException {
+	// Get current rover Position on Orbital Ref, transform to Surf Ref and add 2 meters on height:
+	Vector roverHeight = new Vector(2.0, 0.0, 0.0);
+	Vector currentRoverPos = transformSurfToOrb(new Vector(naveAtual.position(pontoRefSuperficie)).sum(roverHeight));
+	// Calculate distance from rover to target on Orbital Ref:
+	double distanceToTarget = Vector.distance(currentRoverPos, targetPosition);
+	// Add rover pos as first point, on Orbital Ref
+	pathToTarget.add(currentRoverPos);
+	// Calculate the next points positions and add to the list on Orbital Ref
+	int index = 0;
+	while (distanceToTarget > 100) {
+		Vector previousPoint = pathToTarget.get(index);
+		index++;
+		Vector directionToTarget = transformOrbToSurf(targetPosition).subtract(transformOrbToSurf(previousPoint))
+																																 .normalize();
+		Vector calculatedNextPoint =
+						avoidObstacles(transformOrbToSurf(previousPoint), transformDirection(directionToTarget, true));
+		Vector nextPoint = transformSurfToOrb(transformOrbToSurf(getPosOnSurface(calculatedNextPoint)).sum(roverHeight));
+		drawLineBetweenPoints(previousPoint, nextPoint);
+		pathToTarget.add(nextPoint);
+		double distanceBetweenPoints = Vector.distance(previousPoint, nextPoint);
+		distanceToTarget -= distanceBetweenPoints;
+	}
+	pathToTarget.add(getPosOnSurface(targetPosition));
+}
+
+private void drawLineBetweenPoints(Vector pointA, Vector pointB) throws RPCException {
+	Drawing.Line line = drawing.addLine(pointA.toTriplet(), pointB.toTriplet(), pontoRefOrbital, true);
+	line.setThickness(0.5f);
+	line.setColor(new Triplet<>(1.0, 0.5, 0.0));
+}
+
+private Vector avoidObstacles(Vector currentPoint, Vector targetDirection) throws RPCException, IOException {
+	// PONTO REF SUPERFICIE: X = CIMA, Y = NORTE, Z = LESTE;
+	// Distance of the next point in the path to the previous one:
+	double stepDistance = 10.0;
+
+	// Raycast distance in front of rover:
+	double centerDistance = raycastDistance(currentPoint, transformDirection(targetDirection, false),
+					pontoRefSuperficie);
+	double left15degDistance = raycastDistance(currentPoint, transformDirection(targetDirection.sum(L30DEG)
+																																														 .normalize(), false),
+					pontoRefSuperficie);
+	double left30degDistance = raycastDistance(currentPoint, transformDirection(targetDirection.sum(L60DEG)
+																																														 .normalize(), false),
+					pontoRefSuperficie);
+	double right15degDistance = raycastDistance(currentPoint, transformDirection(targetDirection.sum(R30DEG)
+																																															.normalize(), false),
+					pontoRefSuperficie);
+	double right30degDistance = raycastDistance(currentPoint, transformDirection(targetDirection.sum(R60DEG)
+																																															.normalize(), false),
+					pontoRefSuperficie);
+
+	// Calculate the next point direction based on all raycast distances:
+	Vector nextPointDirection = transformDirection(targetDirection, false).multiply(centerDistance)
+//																																				.sum(transformDirection(targetDirection,
+//																																								false).sum(L30DEG)
+//																																																											 .normalize()
+//																																																											 .multiply(left15degDistance))
+//																																				.sum(transformDirection(targetDirection,
+//																																								false).sum(L60DEG)
+//																																																											 .normalize()
+//																																																											 .multiply(left30degDistance))
+//																																				.sum(transformDirection(targetDirection,
+//																																								false).sum(R30DEG)
+//																																																											 .normalize()
+//																																																											 .multiply(right15degDistance))
+//																																				.sum(transformDirection(targetDirection,
+//																																								false).sum(R60DEG)
+//																																																											 .normalize()
+//																																																											 .multiply(right30degDistance))
+																																				.normalize();
+	return transformSurfToOrb(currentPoint.sum(nextPointDirection.multiply(stepDistance)));
+}
+
+public double raycastDistance(Vector currentPoint, Vector targetDirection, SpaceCenter.ReferenceFrame reference) throws RPCException {
+	double searchDistance = 20.0;
+	return Math.min(centroEspacial.raycastDistance(currentPoint.toTriplet(), targetDirection.toTriplet(), reference),
+					searchDistance);
+}
+
+private Vector transformDirection(Vector vector, boolean toSurf) throws RPCException {
+	if (toSurf) {
+		return new Vector(centroEspacial.transformDirection(vector.toTriplet(), naveAtual.getReferenceFrame(),
+						pontoRefSuperficie));
+	}
+	return new Vector(centroEspacial.transformDirection(vector.toTriplet(), pontoRefSuperficie,
+					naveAtual.getReferenceFrame()));
+}
+
+private Vector getPosOnSurface(Vector vector) throws RPCException {
+	return new Vector(currentBody.surfacePosition(currentBody.latitudeAtPosition(vector.toTriplet(), pontoRefOrbital),
+					currentBody.longitudeAtPosition(vector.toTriplet(), pontoRefOrbital), pontoRefOrbital));
+}
+
+private Vector transformSurfToOrb(Vector vector) throws IOException, RPCException {
+	return new Vector(centroEspacial.transformPosition(vector.toTriplet(), pontoRefSuperficie, pontoRefOrbital));
+}
+
+private Vector transformOrbToSurf(Vector vector) throws IOException, RPCException {
+	return new Vector(centroEspacial.transformPosition(vector.toTriplet(), pontoRefOrbital, pontoRefSuperficie));
+}
+}
