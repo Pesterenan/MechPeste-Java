823770e7d058fe98c7120075e8fc4e5d6acfba9a
Pesterenan <pesterenan@gmail.com>
2023-12-04 10:50:32 -0300

    [Maven]: Configure project to use Maven to deploy new versions

--- src/main/java/com/pesterenan/resources/Bundle.java
+++ src/main/java/com/pesterenan/resources/Bundle.java
@@ -5,9 +5,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Bundle {
-	public static final String BUNDLE_NAME = Bundle.class.getPackage().getName() + ".MechPesteBundle"; //$NON-NLS-1$
-
-	public static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("main.resources.MechPesteBundle",
+	public static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("MechPesteBundle",
 			Locale.getDefault());
 
 	private Bundle() {
